69aa981f72cf7f382e82082da670f174529c2726
Nikolay-zhukau <koliajukov@gmail.com>
2022-12-14 18:22:01 +0300

    Finished module 4 - loops / task 4

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        for (int i = 1; i <= numberToGoUntil; i++) {
+            if (toBreakWith > numberToGoUntil) {
+                System.out.println("iterating till the end");
+                do {
+                    System.out.println(i);
+                    i++;
+                } while (i <= numberToGoUntil);
+                break;
+            }
+            if(i > toBreakWith){
+                continue;
+            }
+                System.out.println(i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,5 +3,33 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+        for (int i = 0; i < Math.abs(lastInRow); i++){
+            int sumAfterSkip = 0;
+            int sumSkip = 0;
+            if (lastInRow < 0) {
+                System.out.println("last number in row is negative");
+                break;
+            }
+            if (lastInRow < numberToSkip) {
+                System.out.println("number to skip is bugger then the last");
+                break;
+            }
+            do {
+                sumSkip = sumSkip + i;
+                i++;
+            }while (i <= numberToSkip);
+            System.out.println("skipped sum is " + sumSkip);
+            if (numberToSkip == lastInRow){
+                System.out.println("counted sum is " + sumAfterSkip);
+                break;
+            }
+            do {
+                sumAfterSkip += i;
+                i++;
+            }while (i > numberToSkip && i <= lastInRow);
+             System.out.println("counted sum is " + sumAfterSkip);
+
+        }
+
     }
 }
